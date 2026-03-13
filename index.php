fb6adf79aca286d0617d11567bef53a3177e562e
Dorival-Camara <garfixsd@gmail.com>
2026-03-13 13:48:51 -0300

    Formulario parte do Html

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comercial</title>
+</head>
+<body>
+
+<h2>Login</h2>
+
+<form method="Post">
+    Usuário : <input type="text" name="usuario"><br><br>
+    Senha : <input type="passaword" name="senha"><br><br>
+    <button type="submit">Entrar</button>
+</form>
+
+<br>
+<strong><?php echo $mensagem; ?></strong>
+    
+</body>
+</html>
+<?php
+
+?>
